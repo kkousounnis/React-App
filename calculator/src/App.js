98a894b8f7c5ac00b9143c89d60f104010c4a35a
Konstantinos <kwstas654321@gmail.com>
2021-01-18 15:31:11 +0200

    Initialize calculator

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import Calculator from './components/calculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/components/calculator.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Calculator(){
+
+    return (
+        <div>Hello let's do it</div>
+    );
+}
+
+export default Calculator;
