9111a7840e1c67facbfdc564890126fdca6c1091
Konstantinos <kwstas654321@gmail.com>
2021-01-18 12:49:40 +0200

    Count Clicks With Hooks

--- myfirst-app/src/App.js
+++ myfirst-app/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = 'You clicked ${count}'
+
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello class Advance js Progrmming</h1>
+
+      <p> You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Button
+     </button>
     </div>
   );
 }
